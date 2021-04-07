23b2cc7378374ca0ec66be75a118ee52992fd768
xiaopifeng <lichunfang3254@gmail.com>
2021-04-07 13:20:03 +0800

    删除注释内容

--- main.c
+++ main.c
@@ -4,7 +4,6 @@ int main(int argc, char **argv)
 {
 	double money;
 	double temp = 0.0;
-	//int money;
 	FILE *fp;
 	if ((fp = fopen((argv[1]),"r+")) == NULL )
 	{	
@@ -13,13 +12,8 @@ int main(int argc, char **argv)
 	}
 	while(fscanf(fp,"%lf",&money) != EOF)
 	{
-	//	printf("%f\n",money);
 		temp = temp + money;
 	}
-	//while(fscanf(fp, "%d", &money) != EOF)
-	//{
-	//	printf("%d\n",&money);
-	//}	
 	fclose(fp);
 	printf("This month %s%f\n",argv[2], temp);	
 	return 0;
